1cef6208d1abaf28d7882a6dadfb96b865b2648d
Ian Riley <ian.riley.915@gmail.com>
2021-05-30 19:17:20 -0500

    Adds template python file with various instructions for accepting inputs on STDIN.

---
+++ _template.py
@@ -0,0 +1,12 @@
+def main():
+    # str = input() # read input string
+    # num = int(input()) # read input integer
+    # num = float(input()) # read input float
+    # nums = [int(i) for i in input().split()] # read sequence of integers
+    # nums = [float(d) for d in input().split()] # read sequence of floats
+    # mtx = [[int(i) for i in input().split()] for _ in range(N)] # read matrix of integers
+    # mtx = [[float(d) for d in input().split()] for _ in range(N)] # read matrix of floats
+    raise NotImplementedError('No implemented solution')
+
+if __name__ == '__main__':
+    main()
